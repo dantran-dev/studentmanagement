3b12c4d6ca2d31c185857e50a909a37150db7e22
Dan Tran <dantr.qld@gmail.com>
2021-04-21 13:10:11 +1000

    edited files

--- stringcmpr.c
+++ stringcmpr.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
 	char str1[] = "abcd", str2[] = "abCd", str3[] = "abcd";
-	int result;
+	int result1, result2;
 
 
-	char* courseName1, *courseName2, *courseName3;
+	char* courseName1, *courseName2, *courseName3, *courseName4;
 
 	courseName1 = malloc(sizeof * courseName1);
 	
@@ -12,20 +16,35 @@ int main()
 
 	courseName3 = malloc(sizeof * courseName3);
 
+	courseName4 = malloc(sizeof *courseName4);
 
-	courseName1 = "IT";
 
-	courseName2 = "BSN";
+	courseName1 = "ABC";
 
-	courseName3 = "LAW";
+	courseName2 = "DEF";
+
+	courseName3 = "MACDONALD";
+
+	courseName4 = "KFC";
+
+
 
 	// comparing strings str1 and str2
-	result = strcmp(courseName1, courseName2);
-	printf("strcmp(str1, str2) = %d\n", result);
+	result1 = strcmp(courseName1, courseName4);
+	printf("strcmp(str1, str4) = %d\n", result1);
 
 	// comparing strings str1 and str3
-	result = strcmp(courseName1, courseName3);
-	printf("strcmp(str1, str3) = %d\n", result);
+	result2 = strcmp(courseName2, courseName4);
+	printf("strcmp(str2, str4) = %d\n", result2);
+
+	if(result1 > result2)
+	{
+		printf("result 1 > result2 \n");
+	}
+	else
+	{
+		printf("result2 > result 1 \n");
+	}
 
 	return 0;
 }
--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef char *String;
 
@@ -16,7 +17,7 @@ typedef struct list
 {
     ListNodePtr head;
 
-} StudentList;
+} *StudentList;
 
 typedef struct course
 {
@@ -30,13 +31,14 @@ typedef struct bstNode
     Course course;
     struct bstNode *left;
     struct bstNode *right;
-} * BSTNodePtr;
+} *BSTNodePtr;
 
 //root of the BST (Course Structure)
 
 typedef struct bst
 {
     BSTNodePtr root;
+
 } CourseBST;
 
 BSTNodePtr testInsert(BSTNodePtr self, String courseName)
@@ -46,43 +48,159 @@ BSTNodePtr testInsert(BSTNodePtr self, String courseName)
     Course newCourse;
     newCourse = malloc(sizeof *newCourse);
 
-    StudentList newStudent;
-    newStudent = malloc(sizeof *newStudent);
+    StudentList newStudentList;
+    newStudentList = malloc(sizeof *newStudentList);
 
-    newCourse->name = "IT";
-    newCourse->student = newStudent;
+    newStudentList->head = NULL;
 
-    if (sell == NULL)
+    //courseName = (char*)malloc(sizeof *courseName);
+
+   
+    newCourse->name = courseName;
+    newCourse->student = newStudentList;
+
+    if (self == NULL)
     {
         self = malloc(sizeof *self);
         self->course = newCourse;
         self->left = self->right = NULL;
     }
-    else
+    /*else if (self != NULL)
+    {
+        printf("binary tree not null\n");
+        int result;
+
+        result = strcmp(courseName, self->course->name);
+        printf("\n %d", result);
+
+    }*/
+    else if (strcmp(self->course->name, newCourse->name) > 0)
     {
         self->right = testInsert(self->right, courseName);
     }
+        
+    else if(strcmp(self->course->name, newCourse->name) < 0)
+    {
+        self->left = testInsert(self->left, courseName);
+      
+    }
     return self;
 }
+
 
 void insertCourse(CourseBST *self, String courseName)
 {
     self->root = testInsert(self->root, courseName);
 }
 
-CourseBST newBST()
-{
-    CourseBST temp;
-    temp.root = NULL;
-    return temp;
+
+void print_pre_order_bst_node(BSTNodePtr self) {
+	if (self != NULL) {
+	
+		printf("%s\n", self->course->name);
+		print_pre_order_bst_node(self->left);
+		print_pre_order_bst_node(self->right);
+		
+	}
+	
+}
+void print_pre_order_bst(CourseBST* self) {
+	print_pre_order_bst_node(self->root);
 }
 
+BSTNodePtr min_node(BSTNodePtr self) {
+	BSTNodePtr current = self;
+	while (current->left != NULL) {
+		current = current->left;
+	}
+	return current;
+}
+
+BSTNodePtr deleteCourse(BSTNodePtr self, String courseName)
+{
+    if (self != NULL) {
+		if (strcmp(self->course->name, courseName) == 0) { // found item 
+			if (self->left != NULL && self->right != NULL) {
+				// two child case 
+				BSTNodePtr successor = min_node(self->right);
+				self->course->name = successor->course->name;
+				self->right = deleteCourse(self->right, self->course->name);
+			}
+			else { // one or zero child case 
+				BSTNodePtr to_free = self;
+				if (self->left) {
+					self = self->left;
+				}
+				else {
+					self = self->right;
+				}
+				free(to_free);
+			}
+		}
+		else if (strcmp(courseName, self->course->name) > 0) {
+			self->left = deleteCourse(self->left, courseName);
+		}
+		else {
+			self->right = deleteCourse(self->right, courseName);
+		}
+	}
+	return self;
+}
+
+
+void delete_bst(CourseBST* self, String courseName) {
+	self->root = deleteCourse(self->root, courseName);
+}
+
+
+void enrollStudent(BSTNodePtr self, String courseName, int studentID)
+{   
+
+    //find the courseName where the student will be enrolled into
+    //if found at the first node, insert student into that node
+    if(self == NULL || (strcmp(courseName, self->course->name) == 0))
+    {
+        
+        //insert into linkedlist by in order
+
+        //self->course->student->head->id = studentID;
+
+        
+    }
+    else if (strcmp(self->course->name, courseName) > 0)
+    {
+        enrollStudent(self->left, courseName, studentID);
+    }
+    else 
+    {
+        enrollStudent(self->right, courseName, studentID);
+    }
+
+}
+
+CourseBST newCourse()
+{
+	CourseBST temp;
+	temp.root = NULL;
+	return temp;
+}
 int main()
 {
 
-    CourseBST myBST = newBST();
+    CourseBST myBST = newCourse();
 
-    insertCourse(myBST, 'iT');
+
+    insertCourse(&myBST, "IT");
+    insertCourse(&myBST, "Nurse");
+    insertCourse(&myBST, "BUSINESS");
+    insertCourse(&myBST, "ART");
+
+    print_pre_order_bst(&myBST);
+
+    printf("after deteled a course\n");
+    delete_bst(&myBST, "IT");
+
+    print_pre_order_bst(&myBST);
 }
 
 //test insert BSTNode
